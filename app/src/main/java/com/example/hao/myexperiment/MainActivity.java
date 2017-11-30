8e10582d7d3ab6655e57d10cd402a6e529d981ae
awmleer <awmleer@sparker.xyz>
2017-11-30 12:04:51 +0800

    http request done

--- app/src/main/java/com/example/hao/myexperiment/MainActivity.java
+++ app/src/main/java/com/example/hao/myexperiment/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.hao.myexperiment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void onStartClicked(View v){
+        final EditText mEditText = (EditText) findViewById(R.id.editText);
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url =mEditText.getText().toString();
 
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,5000));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 
