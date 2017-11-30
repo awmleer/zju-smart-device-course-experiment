c56251fc22f3b1371a0ade62049e5fae6caa346f
awmleer <awmleer@sparker.xyz>
2017-11-30 11:43:53 +0800

    new layout done

--- app/src/main/java/com/example/hao/myexperiment/MainActivity.java
+++ app/src/main/java/com/example/hao/myexperiment/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hao.myexperiment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onStartClicked(View v){
+
     }
 
 
