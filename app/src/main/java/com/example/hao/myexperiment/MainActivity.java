59c7e3048822334752af0fde2a75f2a3585cbc90
awmleer <awmleer@sparker.xyz>
2017-11-30 11:28:17 +0800

    Revert "opengl"
    This reverts commit 01770a346425d3fc81cee8fbb1c26a0880b4e22c.

--- app/src/main/java/com/example/hao/myexperiment/MainActivity.java
+++ app/src/main/java/com/example/hao/myexperiment/MainActivity.java
@@ -1,51 +1,15 @@
 package com.example.hao.myexperiment;
 
-import android.content.Context;
-import android.opengl.EGLConfig;
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.GLU;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
-
-import javax.microedition.khronos.opengles.GL10;
-
 public class MainActivity extends AppCompatActivity {
-
-    private GLSurfaceView myGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myGLView = new MyGLSurfaceView(this);
-        setContentView(myGLView);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
 
 }
-
-class MyGLSurfaceView extends GLSurfaceView {
-
-    private final MyGLRenderer mRenderer;
-
-    public MyGLSurfaceView(Context context){
-        super(context);
-
-        // Create an OpenGL ES 2.0 context
-        setEGLContextClientVersion(2);
-
-        mRenderer = new MyGLRenderer();
-
-        // Set the Renderer for drawing on the GLSurfaceView
-        setRenderer(mRenderer);
-    }
-}
-
-
-
