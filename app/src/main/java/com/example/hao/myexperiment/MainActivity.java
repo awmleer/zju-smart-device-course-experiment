619fdc777a3f25ef2ceb26495876b201df7c4519
awmleer <awmleer@sparker.xyz>
2017-12-04 20:09:42 +0800

    text message

--- app/src/main/java/com/example/hao/myexperiment/MainActivity.java
+++ app/src/main/java/com/example/hao/myexperiment/MainActivity.java
@@ -1,20 +1,20 @@
 package com.example.hao.myexperiment;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String SMS_ACTION = "com.android.TinySMS.RESULT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onStartClicked(View v){
-        final EditText mEditText = (EditText) findViewById(R.id.editText);
-        final TextView mTextView = (TextView) findViewById(R.id.textView);
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url =mEditText.getText().toString();
+    private SentReceiver receiver = new SentReceiver();
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,5000));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
+    public void onStartClicked(View v){
+        EditText addressView = findViewById(R.id.editText);
+        EditText contentView = findViewById(R.id.editText2);
+        this.sendSMS(addressView.getText().toString(),contentView.getText().toString());
+    }
+
+    private class SentReceiver extends BroadcastReceiver {
+        @Override public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(SMS_ACTION)) {
+                int code = getResultCode(); //短消息发送成功
+                if(code == Activity.RESULT_OK) Toast.makeText(MainActivity.this, "发送成功", Toast.LENGTH_SHORT).show();
             }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        }
+    }
+
+    private void sendSMS(String address, String content) {
+        SmsManager manager = SmsManager.getDefault();
+        Intent i = new Intent(SMS_ACTION); //生成PendingIntent，当消息发送完成，接收到广播
+        PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, i, PendingIntent.FLAG_ONE_SHOT);
+        manager.sendTextMessage( address, null, content, sentIntent, null);
     }
 
 
